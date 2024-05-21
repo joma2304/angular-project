5f88738ee3b81a501901e7bed35c13c5aca905fd
Johan Magnusson <joma2304@student.miun.se>
2024-05-21 17:06:39 +0200

    Gjort upp en början på att skal

---
+++ src/app/service/get-courses.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../models/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetCoursesService {
+  url : string = 'https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json';
+
+  constructor(private http: HttpClient) { }
+
+  //Metod för att hämta kurser
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
+}
