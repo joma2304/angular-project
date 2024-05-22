f7c7ab3bedb8bf498a7267f110e93cfd031fcdbf
Johan Magnusson <joma2304@student.miun.se>
2024-05-22 14:36:45 +0200

    Kan ta bort från ramschemat nu med

--- src/app/frame-schedule/frame-schedule.component.ts
+++ src/app/frame-schedule/frame-schedule.component.ts
@@ -10,8 +10,8 @@ import { MatCardModule } from '@angular/material/card';
   selector: 'app-frame-schedule',
   standalone: true,
   imports: [CommonModule,
-            MatTableModule, 
-            MatCardModule],
+    MatTableModule,
+    MatCardModule],
   templateUrl: './frame-schedule.component.html',
   styleUrl: './frame-schedule.component.scss'
 })
@@ -25,6 +25,17 @@ export class FrameScheduleComponent implements OnInit {
       this.savedCourses = courses;
     });
   }
+
+  removeCourse(course: Course): void {
+    const key = course.courseCode; // 
+    localStorage.removeItem(key); // Ta bort kursen från local storage med hjälp av key
+
+    // Uppdatera savedCourses genom att filtrera bort den borttagna kursen
+    this.savedCourses = this.savedCourses.filter(savedCourse => savedCourse.courseCode !== course.courseCode);
+  }
+
+
 }
+
 
 
